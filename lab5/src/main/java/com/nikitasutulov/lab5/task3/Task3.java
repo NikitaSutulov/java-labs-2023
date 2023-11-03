e92f22e3067f1acdd4f14982614c89af884d5923
Nikita Sutulov <notsid212@gmail.com>
2023-11-03 07:53:47 +0200

    lab 5 added typing file pathes from console for task 3

--- lab5/src/main/java/com/nikitasutulov/lab5/task3/Task3.java
+++ lab5/src/main/java/com/nikitasutulov/lab5/task3/Task3.java
@@ -1,15 +1,34 @@
 package com.nikitasutulov.lab5.task3;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Task3 {
 
     public static void main(String[] args) {
         final char KEY = '\2';
 
+        String[] filePathes = new String[3];
+        String[] messages = {
+                "Enter the file path to get the bytes stream from",
+                "Enter the file path to put the encrypted stream to",
+                "Enter the file path to put the decrypted stream to"
+        };
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            for (int i = 0; i < messages.length; i++) {
+                System.out.println(messages[i]);
+                filePathes[i] = scanner.nextLine();
+            }
+        }
+
+        String inputFilePath = filePathes[0];
+        String encryptedFilePath = filePathes[1];
+        String decryptedFilePath = filePathes[2];
+
         try {
-            FileInputStream inputFileStream = new FileInputStream("input.txt");
-            FileOutputStream encryptedFileOutputStream = new FileOutputStream("encrypted.txt");
+            FileInputStream inputFileStream = new FileInputStream(inputFilePath);
+            FileOutputStream encryptedFileOutputStream = new FileOutputStream(encryptedFilePath);
             EncryptionOutputStream encryptionOutputStream = new EncryptionOutputStream(encryptedFileOutputStream, KEY);
 
             int byteRead;
@@ -21,8 +40,8 @@ public class Task3 {
             encryptionOutputStream.close();
             encryptedFileOutputStream.close();
 
-            FileInputStream encryptedFileInputStream = new FileInputStream("encrypted.txt");
-            FileOutputStream decryptedFileOutputStream = new FileOutputStream("decrypted.txt");
+            FileInputStream encryptedFileInputStream = new FileInputStream(encryptedFilePath);
+            FileOutputStream decryptedFileOutputStream = new FileOutputStream(decryptedFilePath);
             DecryptionInputStream decryptionInputStream = new DecryptionInputStream(encryptedFileInputStream, KEY);
 
             int decryptedByte;
