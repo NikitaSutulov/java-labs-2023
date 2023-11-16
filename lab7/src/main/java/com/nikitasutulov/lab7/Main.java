693c78fda6585551ecf3e51447b55362f514f89c
Nikita Sutulov <notsid212@gmail.com>
2023-11-16 21:39:07 +0200

    lab 7 added method pointers examples

--- lab7/src/main/java/com/nikitasutulov/lab7/Main.java
+++ lab7/src/main/java/com/nikitasutulov/lab7/Main.java
@@ -9,25 +9,21 @@ import java.util.function.Predicate;
 public class Main {
     public static Consumer<String[]> printFilteredArray = (array) -> {
         System.out.println("The filtered array is:");
-        Arrays.stream(array).forEach(word -> System.out.print(word + "\t"));
+        Arrays.stream(array).forEach(Main::printWordWithTab);
     };
 
     public static Predicate<Character> isCharLatin = ch -> Character.toString(ch).matches("[A-Za-z]");
 
     public static Function<String[], String[]> filterLatin = (words) ->
             Arrays.stream(words)
-                    .filter(word -> word.chars().allMatch(c -> isCharLatin.test((char) c)))
+                    .filter(Main::isWordLatin)
                     .toArray(String[]::new);
 
     public static Predicate<Character> isCharVowel = ch -> Character.toString(ch).matches("[AIEOUaieou]");
 
     public static Function<String[], String[]> filterVowelsEqualConsonants = (words) ->
             Arrays.stream(words)
-                    .filter(word -> {
-                        long vowelCount = word.chars().filter(c -> isCharVowel.test((char) c)).count();
-                        long consonantCount = word.length() - vowelCount;
-                        return vowelCount == consonantCount;
-                    })
+                    .filter(Main::isVowelsEqualConsonants)
                     .toArray(String[]::new);
 
     public static Function<String, String[]> filterLine = (line) ->
@@ -39,5 +35,20 @@ public class Main {
             String[] filteredWords = filterLine.apply(line);
             printFilteredArray.accept(filteredWords);
         }
+    }
+
+    // methods to show the ability to pass pointers to them as arguments
+    public static boolean isVowelsEqualConsonants(String word) {
+        long vowelCount = word.chars().filter(c -> isCharVowel.test((char) c)).count();
+        long consonantCount = word.length() - vowelCount;
+        return vowelCount == consonantCount;
+    }
+
+    public static boolean isWordLatin(String word) {
+        return word.chars().allMatch(c -> isCharLatin.test((char) c));
+    }
+
+    public static void printWordWithTab(String word) {
+        System.out.print(word + "\t");
     }
 }
